fa04524a822e87ead99b07b97386487f6fd61259
j22807018 <j22807018@yahoo.com.tw>
2016-03-12 23:21:12 +0800

    update model

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'portal_id', 'is_admin'
     ];
 
     /**
@@ -23,4 +23,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function isAdmin()
+    {
+        return ($this->is_admin == true);
+    }
 }
